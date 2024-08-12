5ae230f82ce076bdf962a0b159b4ba41fdfa1df2
sonu kumar <dev.sonukumarsingh@gmail.com>
2024-08-12 20:36:23 +0530

    put method added

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 const menu = require("../model/menu");
 
-// ----- post method for show users -----
-// ----- Get method for show users -----
+// ----- post method for show menu -----
 router.post("/", async (req, res) => {
   try {
     const newMenu = await menu.create(req.body);
@@ -14,6 +13,7 @@ router.post("/", async (req, res) => {
   }
 });
 
+// ----- Get method for show menu -----
 router.get("/", async (req, res) => {
   try {
     const menus = await menu.find({});
@@ -24,6 +24,7 @@ router.get("/", async (req, res) => {
   }
 });
 
+// ----- Get method for show filter menu -----
 router.get("/:taste", async (req, res) => {
   try {
     const menus = await menu.find({ taste: req.params.taste });
@@ -31,6 +32,22 @@ router.get("/:taste", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// ----- put method for update menu -----
+
+router.put("/:id", async (req, res) => {
+  try {
+    const menuToUpdate = await menu.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!menuToUpdate)
+      return res.status(404).json({ message: "Menu not found" });
+    res.json(menuToUpdate);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: `Internal server error -  ${error}` });
   }
 });
 
